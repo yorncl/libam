98d313a3f2a2f0a7de4d1c6ea7fe2c96092ed940
Martin CLAUDEL <mclaudel@e1r12p6.42.fr>
2020-03-10 12:11:14 +0100

    doing test cases

--- libasm.h
+++ libasm.h
@@ -6,7 +6,7 @@
 /*   By: mclaudel <mclaudel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 14:23:25 by mclaudel          #+#    #+#             */
-/*   Updated: 2020/02/11 17:04:48 by mclaudel         ###   ########.fr       */
+/*   Updated: 2020/03/10 11:55:07 by mclaudel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <stdlib.h>
 
 int		ft_strlen(char *str);
-int		ft_strcpy(char *dst, char *src);
+char	*ft_strcpy(char *dst, char *src);
 int		ft_strcmp(char *s1, char *s2);
 int		ft_write(int fildes, const void *buf, size_t nbyte);
 size_t	ft_read(int fildes, void *buf, size_t nbyte);
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: mclaudel <mclaudel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 14:23:30 by mclaudel          #+#    #+#             */
-/*   Updated: 2020/02/11 17:23:32 by mclaudel         ###   ########.fr       */
+/*   Updated: 2020/03/10 12:08:01 by mclaudel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,70 @@
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 void	strcmp_test(char *s1, char *s2)
 {
-	printf("%d\t %d\n", strcmp(s1, s2), ft_strcmp(s1, s2));
-	printf("%d\t %d\n", strcmp(s1, s2), ft_strcmp(s1, s2));
+	printf("%-10d vs %10d\n", strcmp(s1, s2), ft_strcmp(s1, s2));
 }
 
-int main(void)
+void	strcy_test(char *src)
 {
-	// printf("%d\n", ft_strlen(""));
-	// printf("%d\n", ft_strlen("\n"));
-	// printf("%d\n", ft_strlen("a"));
-	// printf("%d\n\n", ft_strlen("Hello"));
+	char dest1[11];
+	char dest2[11];
+	char *r1;
+	char *r2;
 
-	// char src [11] = "HELLO MOTO";
-	// char dest [11];
-	// ft_strcpy(dest, src);
-	// printf("|%p|\n", src);
-	// printf("|%s|\n", src);
-	// printf("|%p|\n", dest);
-	// printf("|%s|\n\n", dest);
+	r1 = strcpy(dest1, src);
+	r2 = ft_strcpy(dest2, src);
+	printf("src: \"%s\"\n\tdest1:|%s|\tr1=%p vs dst1=%p\n\tdest2:|%s|\tr2=%p vs dst2=%p\n",
+											src, dest1, r1, dest1, dest2, r2, dest2);
+}
 
-	// strcmp_test("", "");
-	// strcmp_test("a", "a");
-	// strcmp_test("a", "aa");
-	// strcmp_test("aaaaa", "");
-	// strcmp_test("", "aaaa");
-	// strcmp_test("asdasdas", "qfoprqqq");
-	// strcmp_test("a", "a\200");
-	
-	// int r = ft_write(1, "ft_write test plz\n", 18);
-	// printf("%d\n", r);
-	// r = ft_write(1, "ft_write test plz\n", 18);
-	// printf("%d\n", r);
+void	write_test(int fd, char *buff, int nb)
+{
+	int r1;
+	int r2;
 
-	// printf("\n\n\n\n\n");
-	
+	printf("-------------------\n");
+	r1 = ft_write(fd, buff, nb);
+	r2 = write(fd, buff, nb);
+	printf("%-7d vs %7d\n", r1, r2);
+}
+
+int		main(void)
+{
+	printf("\e[1;34m======= ft_strlen =======\e[0m\n");
+	printf("%-10d vs %10lu\n", ft_strlen(""), strlen(""));
+	printf("%-10d vs %10lu\n", ft_strlen("\n"), strlen("\n"));
+	printf("%-10d vs %10lu\n", ft_strlen("a"), strlen("a"));
+	printf("%-10d vs %10lu\n", ft_strlen("Hello"), strlen("Hello"));
+	printf("%-10d vs %10lu\n", ft_strlen("Hel\0lo"), strlen("Hel\0lo"));
+
+	printf("\e[1;34m======= ft_strcpy =======\e[0m\n");
+	strcy_test("hello moto");
+	strcy_test("");
+	strcy_test("aaaa\0aaaa");
+
+	printf("\e[1;34m======= ft_strcmp =======\e[0m\n");
+	strcmp_test("", "");
+	strcmp_test("a", "a");
+	strcmp_test("a", "aa");
+	strcmp_test("aaaaa", "");
+	strcmp_test("", "aaaa");
+	strcmp_test("asdasdas", "qfoprqqq");
+	strcmp_test("a", "a\200");
+
+	printf("\e[1;34m======= ft_write =======\e[0m\n");
+	write_test(1, "ft_write test plz\n", 18);
+	write_test(666, "ft_write test plz\n", 18);
+	write_test(1, "", 0);
+	write_test(666, "", 0);
+	write_test(1, "ft_write test plz\n", -18);
+	write_test(666, "ft_write test plz\n", -18);
+
+	printf("\e[1;34m======= ft_read =======\e[0m\n");
+	// printf("\n\n\n\n\n");	
 	// char buff[100];
 	// int fd = open("libasm.h", O_RDONLY);
 	// bzero(buff, 100);
@@ -61,6 +88,7 @@ int main(void)
 	// }
 	// printf("END OF TESTS\n");
 
+	printf("\e[1;34m======= ft_strdup =======\e[0m\n");
 	char *str;
 
 	str= ft_strdup("Hello moto");
