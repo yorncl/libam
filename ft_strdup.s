c7f5177007c8924a4b9e6c4ea78a718bf661c3c8
mclaudel <mclaudel@student.42.fr>
2020-07-09 16:48:42 +0200

    fixed ft_strdup

--- ft_strdup.s
+++ ft_strdup.s
@@ -8,22 +8,15 @@ extern ft_strcpy
 section .text
 
 ft_strdup:
-	push rdi
 	call ft_strlen
-	;push rax ;keeping string length
-
+	push rdi
 	mov rdi, rax
 	inc rdi
 	call malloc
 	test rax, rax
 	jz error
-
-;	pop rcx ;popping string length
 	pop rsi
 	mov rdi, rax
-	
-	;mov [rdi + rcx], byte 0  ;null-terminating the string
-	
 	call ft_strcpy
 	ret
 
