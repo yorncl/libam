c1b21642d69f768b1b9e3de0c795e7931c849672
mclaudel <mclaudel@student.42.fr>
2020-07-09 16:50:14 +0200

    normed header

--- libasm.h
+++ libasm.h
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   libasm.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mclaudel <mclaudel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 14:23:25 by mclaudel          #+#    #+#             */
-/*   Updated: 2020/03/11 12:01:14 by mclaudel         ###   ########.fr       */
+/*   Updated: 2020/07/09 16:50:02 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBASM_H
 # define LIBASM_H
 
-#include <stdlib.h>
-#include <sys/types.h>
+# include <stdlib.h>
+# include <sys/types.h>
 
-size_t     	ft_strlen(const char *s);
+size_t		ft_strlen(const char *s);
 char		*ft_strcpy(char *dst, char *src);
 int			ft_strcmp(char *s1, char *s2);
-ssize_t     ft_write(int fildes, const void *buf, size_t nbyte);
-ssize_t     ft_read(int fildes, void *buf, size_t nbyte);
+ssize_t		ft_write(int fildes, const void *buf, size_t nbyte);
+ssize_t		ft_read(int fildes, void *buf, size_t nbyte);
 char		*ft_strdup(const char *s1);
 #endif
